1423fc2f857857203efea28b936a18da57d652ca
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-24 19:33:43 +0300

    modify about page styles

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,98 +1,12 @@
 "use client";
-
-// icons
 import { useState } from "react";
-import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
-} from "react-icons/fa";
-
-import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from "react-icons/si";
 import { fadeIn } from "@/variants";
 import { motion } from "framer-motion";
 import Avatar from "@/components/Avatar";
 import Circles from "@/components/Circles";
 import CountUp from "react-countup";
 //  data
-const aboutData = [
-  {
-    title: "skills",
-    info: [
-      {
-        title: "Web Development",
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
-        ],
-      },
-      {
-        title: "UI/UX Design",
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: "awards",
-    info: [
-      {
-        title: "Webby Awards - Honoree",
-        stage: "2011 - 2012",
-      },
-      {
-        title: "Adobe Design Achievement Awards - Finalist",
-        stage: "2009 - 2010",
-      },
-    ],
-  },
-  {
-    title: "experience",
-    info: [
-      {
-        title: "UX/UI Designer - XYZ Company",
-        stage: "2012 - 2023",
-      },
-      {
-        title: "Web Developer - ABC Agency",
-        stage: "2010 - 2012",
-      },
-      {
-        title: "Intern - DEF Corporation",
-        stage: "2008 - 2010",
-      },
-    ],
-  },
-  {
-    title: "credentials",
-    info: [
-      {
-        title: "Web Development - ABC University, LA, CA",
-        stage: "2011",
-      },
-      {
-        title: "Computer Science Diploma - AV Technical Institute",
-        stage: "2009",
-      },
-      {
-        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-        stage: "2006",
-      },
-    ],
-  },
-];
+import aboutData from "@/constants/aboutData";
 
 const About = () => {
   const [index, setIndex] = useState(0);
@@ -105,13 +19,13 @@ const About = () => {
         initial="hidden"
         animate="show"
         exit="hidden"
-        className=" hidden xl:flex absolute bottom-0 -left-[370px]"
+        className=" hidden xl:flex absolute bottom-0 -left-[370px] "
       >
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text */}
-        <div className="flex-1 flex flex-col justify-center">
+        <div className="flex-1 flex flex-col justify-center gap-2  z-10">
           <motion.h2
             variants={fadeIn("right", 0.2)}
             initial="hidden"
@@ -119,19 +33,22 @@ const About = () => {
             exit="hidden"
             className="h2"
           >
-            Captivating <span className="text-accent">stories</span> birth
-            magnificent Designs
+            Driven by
+            <span className="text-blue-600"> innovation</span> <br />
+            defined by Code.
           </motion.h2>
           <motion.p
             variants={fadeIn("right", 0.4)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-8 px-2 xl:px-0"
           >
-            7 years ago, I began freelancing as a developer. Since then, I have
-            done remote work for agencies, consulted for startups, and
-            collaborated on digital products for business and consumer use.
+            As a passionate Software Engineer, I thrive on transforming ideas
+            into captivating experiences. With a keen eye for detail and a love
+            for innovative solutions.
+            <br /> Explore my portfolio to witness the journey of turning
+            concepts into compelling realities.
           </motion.p>
           {/* counters */}
           <motion.div
@@ -144,8 +61,8 @@ const About = () => {
             <div className="flex flex-1 xl:gap-x-6">
               {/* experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={10} duration={5} />+
+                <div className="text-2xl xl:text-4xl font-extrabold text-blue-600 mb-2">
+                  <CountUp start={0} end={4} duration={5} />+
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Years of experience
@@ -153,8 +70,8 @@ const About = () => {
               </div>
               {/* clients */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={50} duration={5} />+
+                <div className="text-2xl xl:text-4xl font-extrabold text-blue-600 mb-2">
+                  <CountUp start={0} end={7} duration={5} />+
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Satisfied clients
@@ -162,8 +79,8 @@ const About = () => {
               </div>
               {/* projects */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={80} duration={5} />+
+                <div className="text-2xl xl:text-4xl font-extrabold text-blue-600 mb-2">
+                  <CountUp start={0} end={16} duration={5} />+
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Finished projects
@@ -171,8 +88,8 @@ const About = () => {
               </div>
               {/* Awards */}
               <div className="relative flex-1 ">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={5} duration={5} />+
+                <div className="text-2xl xl:text-4xl font-extrabold text-blue-600 mb-2">
+                  <CountUp start={0} end={3} duration={5} />+
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Winning awards
@@ -186,7 +103,7 @@ const About = () => {
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+          className="flex  flex-col w-full xl:max-w-[48%] h-[480px]"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
@@ -195,7 +112,7 @@ const About = () => {
                   key={itemIndex}
                   className={`${
                     index === itemIndex &&
-                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                    "text-blue-600 after:bg-blue-600 after:w-[100%] after:transition-all after:duration-300"
                   } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
                   onClick={() => setIndex(itemIndex)}
                 >
@@ -204,7 +121,7 @@ const About = () => {
               );
             })}
           </div>
-          <div className="bg-pink-400/10 py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:item-start">
+          <div className="bg-pink-400/10  py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:item-start">
             {aboutData[index].info.map((item, itemIndex) => {
               return (
                 <div
