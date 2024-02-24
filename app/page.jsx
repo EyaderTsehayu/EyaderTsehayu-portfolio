b2979b0e3afa48a8d8abb08ca86819036b6cc4fe
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-24 14:05:00 +0300

    modify home page styling

--- app/page.jsx
+++ app/page.jsx
@@ -17,8 +17,8 @@ export default function Home() {
             exit="hidden"
             className=" h1"
           >
-            Transforming Ideas <br />
-            Into <span className="text-accent">Digital Reality</span>{" "}
+            Ideas Engineered <br />
+            Into <span className="text-blue-600">Digital Horizons</span>{" "}
           </motion.h1>
           <motion.p
             variants={fadeIn("down", 0.3)}
@@ -48,7 +48,7 @@ export default function Home() {
       </div>
 
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
-        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>{" "}
+        <div className="bg-none  xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>{" "}
         <ParticlesContainer />
         <motion.div
           variants={fadeIn("up", 0.5)}
@@ -56,7 +56,7 @@ export default function Home() {
           animate="show"
           exit="hidden"
           transition={{ duration: 1, ease: "easeInOut" }}
-          className=" w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+          className=" w-1/2 h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
         >
           <Avatar />
         </motion.div>
