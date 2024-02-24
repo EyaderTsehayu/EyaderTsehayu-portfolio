4cc4992c0d30c250fde2c6192b67c09bcfa68015
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-24 19:34:33 +0300

    modify projects page styles

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,51 @@
-import React from "react";
-
+"use client";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variants";
+import Circles from "@/components/Circles";
+import WorkSlider from "@/components/WorkSlider";
+import Bulb from "@/components/Bulb";
 const Projects = () => {
   return (
-    <div>
-      <p className=" text-white ">Projects</p>
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-12"
+            >
+              My <span className="text-blue-600"> Projects</span>.
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              Delve into my portfolio for an exclusive look at a compilation of
+              successfully executed projects. Welcome to a showcase of
+              achievements!
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            {/* slider */}
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
     </div>
   );
 };
