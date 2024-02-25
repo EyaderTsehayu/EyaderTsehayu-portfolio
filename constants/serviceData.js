db7b35ede93a364cf2fbbb95b71e7aa22cf37893
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-25 22:53:44 +0300

    put service data separately

---
+++ constants/serviceData.js
@@ -0,0 +1,24 @@
+// icons
+import { RxDesktop, RxRocket, RxMobile } from "react-icons/rx";
+
+const serviceData = [
+  {
+    icon: <RxDesktop />,
+    title: "Web development",
+    description:
+      " From concept to code, I specialize in creating dynamic and responsive websites.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile development",
+    description:
+      " Whether for iOS or Android, I create intuitive and functional mobile apps.",
+  },
+  {
+    icon: <RxRocket />,
+    title: "SEO",
+    description:
+      "Let's implement & enhance your online presence and drive measurable results.",
+  },
+];
+export default serviceData;
