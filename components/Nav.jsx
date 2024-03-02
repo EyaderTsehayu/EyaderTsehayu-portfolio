2c2738c1eb6869f25a2d90b2a69da666fb967360
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-02 17:46:35 +0300

    make the nav responsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,68 +1,115 @@
 // icons
 "use client";
+import { useState } from "react";
+import { FaBars, FaDownload } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import { IoHeart } from "react-icons/io5";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {
-  HiHome,
-  HiUser,
-  HiViewColumns,
-  HiRectangleGroup,
-  HiChatBubbleBottomCenterText,
-  HiEnvelope,
-} from "react-icons/hi2";
-import { FaDownload } from "react-icons/fa6";
-
-// nav data
-export const navData = [
-  { name: "home", path: "/", icon: <HiHome /> },
-  { name: "about", path: "/about", icon: <HiUser /> },
-  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
-  { name: "work", path: "/work", icon: <HiViewColumns /> },
-  {
-    name: "testimonials",
-    path: "/testimonials",
-    icon: <HiChatBubbleBottomCenterText />,
-  },
-  {
-    name: "contact",
-    path: "/contact",
-    icon: <HiEnvelope />,
-  },
-];
 
 const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
-    <div className="hidden xl:flex items-center gap-x-5 text-lg">
-      <Link
-        href={"https://www.linkedin.com/in/eyadertsehayu/"}
-        className="hover:text-accent transition-all duration-300"
-      >
-        Home
-      </Link>{" "}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        About
-      </Link>{" "}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        Portfolio
-      </Link>{" "}
-      {/* <Link href={""} className="hover:text-accent transition-all duration-300">
-        Services
-      </Link>{" "}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        Testimonials
-      </Link> */}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        Contact
-      </Link>{" "}
-      <Link
-        href={""}
-        className="flex flex-row gap-2 items-center bg-blue-600 rounded-lg hover:bg-blue-500 py-2 px-3  transition-all duration-300"
-      >
-        Resume <FaDownload />
-      </Link>{" "}
+    <div className="flex items-center justify-between xl:flex xl:items-center xl:gap-x-5 text-lg xl:pb-3">
+      <div className="hidden xl:flex items-center gap-x-7">
+        <Link
+          href={"/"}
+          className="hover:text-blue-600  transition-all duration-300"
+        >
+          Home
+        </Link>{" "}
+        <Link
+          href={"/about"}
+          className="hover:text-blue-600  transition-all duration-300"
+        >
+          About
+        </Link>{" "}
+        <Link
+          href={"/projects"}
+          className="hover:text-blue-600  transition-all duration-300"
+        >
+          Portfolio
+        </Link>{" "}
+        <Link
+          href={"contact"}
+          className="hover:text-blue-600  transition-all duration-300"
+        >
+          Contact
+        </Link>{" "}
+        <button
+          onClick={() => {}}
+          className="flex flex-row gap-2 items-center bg-blue-600 rounded-lg hover:bg-blue-500 py-2 px-3  transition-all duration-300"
+        >
+          Resume <FaDownload />
+        </button>{" "}
+      </div>
+
+      <div className="xl:hidden absolute xl:right-5 right-8 top-10  flex items-center">
+        <FaBars
+          className="text-2xl cursor-pointer"
+          onClick={toggleMobileMenu}
+        />
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="xl:hidden fixed top-0 left-0 w-full h-full bg-primary/95 z-50">
+          <div className="  flex justify-end p-4">
+            <FaXmark
+              className=" text-2xl cursor-pointer  "
+              onClick={toggleMobileMenu}
+            />
+          </div>
+          <div className="flex flex-col items-center gap-y-4">
+            <Link
+              onClick={toggleMobileMenu}
+              href={"/"}
+              className="hover:text-blue-600  transition-all duration-300"
+            >
+              Home
+            </Link>
+            <Link
+              href={"/about"}
+              onClick={toggleMobileMenu}
+              className="hover:text-blue-600  transition-all duration-300"
+            >
+              About
+            </Link>
+            <Link
+              onClick={toggleMobileMenu}
+              href={"/projects"}
+              className="hover:text-blue-600  transition-all duration-300"
+            >
+              Portfolio
+            </Link>
+            <Link
+              onClick={toggleMobileMenu}
+              href={"/contact"}
+              className="hover:text-blue-600  transition-all duration-300"
+            >
+              Contact
+            </Link>
+            <button
+              onClick={toggleMobileMenu}
+              className="flex flex-row gap-2 items-center bg-blue-600 rounded-lg hover:bg-blue-500 py-2 px-3  transition-all duration-300"
+            >
+              Resume <FaDownload />
+            </button>
+          </div>
+          {/* <div>
+            <p>
+              Developed with <IoHeart color="blue" size={24} /> by Eyader
+              Tsehayu
+            </p>
+          </div> */}
+        </div>
+      )}
     </div>
   );
 };
