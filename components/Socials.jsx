3447f28c6624714207a19d658fa77956ba0dffde
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-02 17:47:41 +0300

    make it opens on new tab

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,33 +1,86 @@
 import Link from "next/link";
 import {
-  RiYoutubeLine,
-  RiFacebookLine,
   RiInstagramLine,
   RiLinkedinLine,
   RiGithubLine,
   RiTelegramLine,
 } from "react-icons/ri";
+import { SiUpwork } from "react-icons/si";
 const Socials = () => {
   return (
+    // <div className="flex items-center gap-x-5 text-lg">
+    //   <Link
+    //     href={"https://www.linkedin.com/in/eyadertsehayu/"}
+    //     className="hover:text-blue-600  transition-all duration-300"
+    //   >
+    //     <RiLinkedinLine />
+    //   </Link>{" "}
+    //   <Link
+    //     href={"https://github.com/EyaderTsehayu"}
+    //     className="hover:text-blue-600  transition-all duration-300"
+    //   >
+    //     <RiGithubLine />
+    //   </Link>{" "}
+    //   <Link
+    //     href={"https://t.me/EyaderTs"}
+    //     className="hover:text-blue-600  transition-all duration-300"
+    //   >
+    //     <RiTelegramLine />
+    //   </Link>{" "}
+    //   <Link
+    //     href={"https://www.instagram.com/eyader__tsehayu"}
+    //     className="hover:text-blue-600  transition-all duration-300"
+    //   >
+    //     <RiInstagramLine />
+    //   </Link>
+    //   <Link
+    //     href={"https://www.upwork.com/freelancers/~01255b9a0261c21215"}
+    //     className="hover:text-blue-600  transition-all duration-300"
+    //   >
+    //     <SiUpwork />
+    //   </Link>{" "}
+    // </div>
     <div className="flex items-center gap-x-5 text-lg">
-      <Link
-        href={"https://www.linkedin.com/in/eyadertsehayu/"}
-        className="hover:text-accent transition-all duration-300"
+      <a
+        href="https://www.linkedin.com/in/eyadertsehayu/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-600 transition-all duration-300"
       >
         <RiLinkedinLine />
-      </Link>{" "}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      </a>{" "}
+      <a
+        href="https://github.com/EyaderTsehayu"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-600 transition-all duration-300"
+      >
         <RiGithubLine />
-      </Link>{" "}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      </a>{" "}
+      <a
+        href="https://t.me/EyaderTs"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-600 transition-all duration-300"
+      >
         <RiTelegramLine />
-      </Link>{" "}
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      </a>{" "}
+      <a
+        href="https://www.instagram.com/eyader__tsehayu"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-600 transition-all duration-300"
+      >
         <RiInstagramLine />
-      </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
-        <RiFacebookLine />
-      </Link>{" "}
+      </a>
+      <a
+        href="https://www.upwork.com/freelancers/~01255b9a0261c21215"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-600 transition-all duration-300"
+      >
+        <SiUpwork />
+      </a>{" "}
     </div>
   );
 };
