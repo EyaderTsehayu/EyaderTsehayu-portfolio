ca106aecd997c6c0b1cacc5a4c3fe34f078b7f83
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-25 23:35:21 +0300

    add title and github icon

--- components/WorkSlider.jsx
+++ components/WorkSlider.jsx
@@ -6,9 +6,10 @@ import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
 import { Pagination } from "swiper";
-import { BsArrowRight } from "react-icons/bs";
+import { BsArrowRight, BsGithub } from "react-icons/bs";
 import Image from "next/image";
 import workSlides from "@/constants/projectsData";
+import Link from "next/link";
 
 const WorkSlider = () => {
   return (
@@ -27,10 +28,15 @@ const WorkSlider = () => {
               {slide.images.map((image, index) => {
                 return (
                   <div
-                    className="relative rounded-lg overflow-hidden flex items-center justify-center group flex-col"
+                    className="relative rounded-lg overflow-hidden flex  justify-center group flex-col"
                     key={index}
                   >
-                    <div className="flex items-center justify-center relative overflow-hidden group">
+                    <Link
+                      href={`${image.domainUrl}`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      className="flex items-center justify-center relative overflow-hidden group"
+                    >
                       {/* image */}
                       <Image
                         src={image.path}
@@ -45,41 +51,34 @@ const WorkSlider = () => {
                         <div className="flex items-center gap-x-2 text-[13px] tracking[0.2em]">
                           {/* title part 1 */}
                           <div className="delay-100">
-                            <a
-                              href={image.link}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="delay-100"
-                            >
-                              LIVE
-                            </a>
+                            <div className="delay-100">LIVE</div>
                           </div>
                           {/* title part 2 */}
                           <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                            <a
-                              href={image.link}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="delay-100"
-                            >
-                              PROJECT
-                            </a>
+                            <div className="delay-100">PROJECT</div>
                           </div>
                           {/* icon */}
                           <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                            <a
-                              href={image.link}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                              className="delay-100"
-                            >
+                            <div className="delay-100">
                               <BsArrowRight />
-                            </a>
+                            </div>
                           </div>
                         </div>
                       </div>
+                    </Link>
+                    <Link
+                      href={`${image.githubUrl}`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      className="flex flex-1 items-center justify-between font-bold px-2 py-2 mt-2 bg-white/10 hover:bg-white/20"
+                    >
+                      {image.title}
+                      <BsGithub className="text-2xl" />
+                    </Link>
+
+                    <div className=" bg-white/10 px-2 pb-1 text-justify">
+                      {image.description}
                     </div>
-                    <div>{image.description}</div>
                   </div>
                 );
               })}
